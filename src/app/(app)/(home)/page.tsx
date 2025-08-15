a435625bf161ff78a48c53d1dd55e56848895b54
Danny Cheun <dcheun@gmail.com>
2025-08-15 23:19:03 +0800

    05: search filters

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,16 +1,5 @@
-import configPromise from '@payload-config'
-import { getPayload } from 'payload'
-
 const Home = async () => {
-  const payload = await getPayload({
-    config: configPromise,
-  })
-
-  const data = await payload.find({
-    collection: 'categories',
-  })
-
-  return <div>{JSON.stringify(data, null, 2)}</div>
+  return <div>Home Page</div>
 }
 
 export default Home
