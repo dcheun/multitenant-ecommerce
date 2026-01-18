316d24a13298dfc8f4fdc366adf6307c4f73e14b
Danny Cheun <dcheun@gmail.com>
2026-01-18 10:16:02 -0800

    11: products

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -10,6 +10,7 @@ import { fileURLToPath } from 'url'
 import { Categories } from './collections/Categories'
 import { Media } from './collections/Media'
 import { Users } from './collections/Users'
+import { Products } from '@/collections/Products'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -21,7 +22,7 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media, Categories],
+  collections: [Users, Media, Categories, Products],
   // cookiePrefix: 'funroad',
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
