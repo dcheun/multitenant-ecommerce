9ce3bb6cafbeda8995f7a7f2db6a6144bb2d0797
Danny Cheun <dcheun@gmail.com>
2026-02-11 08:21:13 -0800

    17: product page

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -7,6 +7,24 @@ import { Category, Media, Tenant } from '@/payload-types'
 import { baseProcedure, createTRPCRouter } from '@/trpc/init'
 
 export const productsRouter = createTRPCRouter({
+  getOne: baseProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const product = await ctx.db.findByID({
+        collection: 'products',
+        id: input.id,
+        depth: 2,
+      })
+      return {
+        ...product,
+        image: product.image as Media | null,
+        tenant: product.tenant as Tenant & { image?: Media | null },
+      }
+    }),
   getMany: baseProcedure
     .input(
       z.object({
@@ -50,10 +68,10 @@ export const productsRouter = createTRPCRouter({
           less_than_equal: input.maxPrice,
         }
       }
-      
+
       if (input.tenantSlug) {
         where['tenant.slug'] = {
-          equals: input.tenantSlug
+          equals: input.tenantSlug,
         }
       }
 
