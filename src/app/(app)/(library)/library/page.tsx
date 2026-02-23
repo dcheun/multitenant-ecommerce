e99ad587ed51d0fa495364f5a34431d6f33ffee5
Danny Cheun <dcheun@gmail.com>
2026-02-23 15:09:45 -0800

    28: Deployment (#28)

--- src/app/(app)/(library)/library/page.tsx
+++ src/app/(app)/(library)/library/page.tsx
@@ -4,6 +4,8 @@ import { DEFAULT_LIMIT } from '@/constants'
 import LibraryView from '@/modules/library/ui/views/library-view'
 import { getQueryClient, trpc } from '@/trpc/server'
 
+export const dynamic = 'force-dynamic'
+
 const LibraryPage = () => {
   const queryClient = getQueryClient()
   void queryClient.prefetchInfiniteQuery(
