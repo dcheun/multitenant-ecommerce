2326e97d1c8c675afe9627259a7c3b7bc3e1bb49
Danny Cheun <dcheun@gmail.com>
2026-02-23 15:09:11 -0800

    28: Deployment

--- src/app/(app)/(auth)/sign-in/page.tsx
+++ src/app/(app)/(auth)/sign-in/page.tsx
@@ -1,6 +1,9 @@
+import { redirect } from 'next/navigation'
+
 import SignInView from '@/modules/auth/ui/views/sign-in-view'
 import { caller } from '@/trpc/server'
-import { redirect } from 'next/navigation'
+
+export const dynamic = 'force-dynamic'
 
 const SignInPage = async () => {
   const session = await caller.auth.session()
--- src/app/(app)/(auth)/sign-up/page.tsx
+++ src/app/(app)/(auth)/sign-up/page.tsx
@@ -1,6 +1,9 @@
+import { redirect } from 'next/navigation'
+
 import SignUpView from '@/modules/auth/ui/views/sign-up-view'
 import { caller } from '@/trpc/server'
-import { redirect } from 'next/navigation'
+
+export const dynamic = 'force-dynamic'
 
 const SignUpPage = async () => {
   const session = await caller.auth.session()
