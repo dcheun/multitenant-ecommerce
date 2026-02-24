62bf75d78c7653d45e715474eeee6d170ea1de8e
Danny Cheun <dcheun@gmail.com>
2026-02-24 14:37:38 -0800

    32: Search Filters (#32)

--- src/modules/home/ui/components/search-filters/search-input.tsx
+++ src/modules/home/ui/components/search-filters/search-input.tsx
@@ -1,13 +1,15 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { useTRPC } from '@/trpc/client'
 import { useQuery } from '@tanstack/react-query'
 import { BookmarkCheckIcon, ListFilterIcon, SearchIcon } from 'lucide-react'
 import Link from 'next/link'
-import { useState } from 'react'
-import CategoriesSidebar from './categories-sidebar'
+import { useEffect, useState } from 'react'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import CategoriesSidebar from '@/modules/home/ui/components/search-filters/categories-sidebar'
+import { useProductFilters } from '@/modules/products/hooks/use-product-filters'
+import { useTRPC } from '@/trpc/client'
 
 interface Props {
   disabled?: boolean
@@ -15,16 +17,34 @@ interface Props {
 
 const SearchInput = ({ disabled }: Props) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+  const [searchValue, setSearchValue] = useState('')
+  const [filters, setFilters] = useProductFilters()
 
   const trpc = useTRPC()
   const session = useQuery(trpc.auth.session.queryOptions())
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setFilters({ search: searchValue })
+    }, 500)
+
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, [searchValue, setFilters])
 
   return (
     <div className='flex w-full items-center gap-2'>
       <CategoriesSidebar open={isSidebarOpen} onOpenChange={setIsSidebarOpen} />
       <div className='relative w-full'>
         <SearchIcon className='absolute top-1/2 left-3 size-4 -translate-y-1/2 text-neutral-500' />
-        <Input className='pl-8' placeholder='Search products' disabled={disabled} />
+        <Input
+          className='pl-8'
+          placeholder='Search products'
+          disabled={disabled}
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
       </div>
 
       <Button
